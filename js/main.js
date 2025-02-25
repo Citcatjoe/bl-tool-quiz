bc57442b36686c8a43cf40985683be93d993adaf
César Greppin <Citcatjoe@users.noreply.github.com>
2025-02-25 11:52:36 +0100

    included datalayer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 (function ($) {
   //window.scrollTo(0, 0);
   
+  function getParameterByName(name) {
+    const url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
-  setTimeout(function () {
+  const questionDoc = getParameterByName('questionDoc');
+  //alert(questionDoc);
+
+  document.addEventListener("DOMContentLoaded", function () {
+    window.blickDataLayer.push({
+        event: "iframe_impression",
+        iframe_name: "storytelling_cryptolist",
+        iframe_id: "iframe_impression"
+    }); 
+
+    window.blickDataLayer.push({
+        event: "iframe_impression",
+        iframe_name: "Blick Tools client poll",
+        iframe_id: questionDoc
+    });
+
     $("body").addClass("is-visible");
+  });
+
+
+
+  // setTimeout(function () {
+  //   $("body").addClass("is-visible");
     
-  }, 1000);
+  // }, 1000);
 
   
   
